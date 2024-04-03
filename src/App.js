7dbb00099f3ece56641f361519afdefc49600214
SAISHREYANAMNI <shreyanamani@gmail.com>
2024-04-03 13:25:59 +0530

    changed ui

--- src/App.js
+++ src/App.js
@@ -29,27 +29,17 @@ function App() {
       {image ? (
         <NewPost image={image} />
       ) : (
+        
         <div className="newPostCard">
-          <div className="addPost">
-            <img
-              src="https://images.pexels.com/photos/9371782/pexels-photo-9371782.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-              alt=""
-              className="avatar"
-            />
+           
+          <div className="Post">
+          <h2>FACE DETECTION USING FACE API.JS</h2>
+          <p>Face-api is a JavaScript library created by Vincent Mühler., to detect faces via browser. It is built over tensorflow.js core API. It supports Face Detection, Face Recognition, Face Expression, Age, and Gender Detection.</p>
+      <div class="image"><img src="https://user-images.githubusercontent.com/31125521/50575270-f501d080-0dfb-11e9-9676-8f419efdade4.png" height={450} width={600}/></div>
+       <div className="addPost">
+        
             <div className="postForm">
-              {/* <input
-                type="text"
-                placeholder="What's on your mind?"
-                className="postInput"
-              /> */}
-              <label htmlFor="file"> UPLOAD A PICTURE
-                <img
-                  className="addImg"
-                  src="https://cdn.icon-icons.com/icons2/564/PNG/512/Add_Image_icon-icons.com_54218.png"
-                  alt=""
-                />
-               
-                {/* <button>Send</button> */}
+              <label htmlFor="file"> GET STARTED
               </label>
               <input
                 onChange={(e) => setFile(e.target.files[0])}
@@ -59,6 +49,9 @@ function App() {
               />
             </div>
           </div>
+          </div>
+        <div class="post2">        
+        </div>
         </div>
       )}
     </div>
